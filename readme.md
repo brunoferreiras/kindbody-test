888bbb5c7c98b728af836e5c63b1d37a466232c9
Bruno Ferreira <fs.brunoferreira@gmail.com>
2023-06-30 18:07:24 -0300

    docs: add info about the code

---
+++ readme.md
@@ -0,0 +1,29 @@
+# KindBody Test
+
+## How to use
+
+1. Start the ruby container with `make up`
+```bash
+make up
+```
+
+2. Enter the ruby container with `make bash`
+```bash
+make bash
+```
+
+3. Install the dependencies with `bundle install`
+```bash
+bundle install
+```
+
+4. Run the tests with `make test`
+
+```bash
+make test
+```
+
+## Docs
+It was added comments to the code to explain the logic behind the solution.
+
+That's it!
--- zip_code_endpoint.rb
+++ zip_code_endpoint.rb
@@ -1,15 +1,24 @@
+# This constant is an array of strings that represents the different tiers. It is frozen to prevent modification.
 TIER_VALUES = ['A', 'B', 'C'].freeze
 
+=begin
+This class represents an endpoint for retrieving zip codes within a certain radius. It has the following class variables:
+- `zip_code`: The starting zip code
+- `radius`: The radius within which to search for zip codes
+- `units`: The units of measurement for the radius
+=end
 class ZipCodeEndpoint
   class << self
     attr_accessor :zip_code, :radius, :units
     attr_reader :conn
+    # This method is the constructor for the ZipCodeEndpoint class. It initializes the `conn` instance variable with a new instance of the Faraday class, which is used to make HTTP requests.
     def initialize
       @conn = Faraday.new(
         url: "https://www.zipcodeapi.com/rest/#{ENV['ZIP_CODE_API_KEY']}",
         headers: { 'Content-Type' => 'application/json' }
       )
     end
+    # This method retrieves a list of zip codes within the specified radius from the starting zip code. It takes three parameters: `zipcode`, `radius`, and `units` (optional, defaults to 'mile').
     def get_zip_in_radius(zipcode, radius, units = 'mile')
       @zip_code = zipcode
       @radius = radius
@@ -29,6 +38,7 @@ class ZipCodeEndpoint
 
     private
 
+    # This private method takes two parameters: `sorted_zips` (an array of zip codes) and `zip_codes_with_distance` (a hash mapping zip codes to distances). It formats the zip codes and distances into a response object with the following attributes: `name`, `address`, `city`, `state`, `distance`, `tier`, `contact_email`, and `contact_name`.
     def format_response(sorted_zips, zip_codes_with_distance)
       sorted_zips.map do |item|
         {
@@ -44,6 +54,7 @@ class ZipCodeEndpoint
       end
     end
 
+    # This private method retrieves the zip codes and distances from the API. It calls the `call_zip_codes_api` method to make a POST request to the API and parses the response. It returns a hash with two keys: `zip_codes` (an array of zip codes) and `zip_codes_with_distance` (a hash mapping zip codes to distances).
     def extract_zip_codes_from_api()
       zip_data = call_zip_codes_api(zip_code, radius, units)
       return nil unless zip_data
@@ -59,6 +70,7 @@ class ZipCodeEndpoint
       { 'zip_codes' => zip_codes, 'zip_codes_with_distance' => zip_codes_with_distance }
     end
 
+    # This private method makes a POST request to the API to retrieve the zip codes and distances within the specified radius from the starting zip code. It uses the `conn` instance variable to make the request and returns the parsed response body as a JSON object.
     def call_zip_codes_api()
       response = @conn.post("/radius.json/#{@zip_code}/#{@radius}/#{@units}")
       return nil unless response.success?
@@ -66,14 +78,17 @@ class ZipCodeEndpoint
       JSON.parse(response.body)
     end
 
+    # This private method retrieves the zip codes from the database that match the zip codes returned from the API. It takes one parameter: `zipcodes` (an array of zip codes). It queries the PartnerClinic model to find zip codes that are in the `zipcodes` array and have a tier value in the `TIER_VALUES` constant. It orders the results by tier in ascending order and returns the result collection.
     def extract_zip_codes_from_db(zipcodes)
       PartnerClinic.where('zipcode IN (?)', zipcodes).where('tier IN (?)', TIER_VALUES).order('tier ASC')
     end
 
+    # This private method sorts the zip codes from the database by distance. It takes two parameters: `zip_codes_with_distance` (a hash mapping zip codes to distances) and `zip_codes_from_db` (an array of zip codes from the database). It calls the `quicksort` method to perform the sorting.
     def sort_extracted_zips_from_db_by_distance(zip_codes_with_distance, zip_codes_from_db)
       quicksort(zip_codes_from_db, 0, zip_codes_from_db.length - 1, zip_codes_with_distance)
     end
 
+    # This private method implements the quicksort algorithm to sort the zip codes by distance. It takes four parameters: `zip_codes` (an array of zip codes), `low` (the starting index), `high` (the ending index), and `zip_codes_with_distance` (a hash mapping zip codes to distances). It recursively partitions the array and sorts the partitions.
     def quicksort(zip_codes, low, high, zip_codes_with_distance)
       if low < high
         pivot_index = partition(zip_codes, low, high, zip_codes_with_distance)
@@ -81,7 +96,8 @@ class ZipCodeEndpoint
         quicksort(zip_codes, pivot_index + 1, high, zip_codes_with_distance)
       end
     end
-    
+
+    # This private method partitions the zip codes array for the quicksort algorithm. It takes four parameters: `zip_codes` (an array of zip codes), `low` (the starting index), `high` (the ending index), and `zip_codes_with_distance` (a hash mapping zip codes to distances). It selects a pivot element and rearranges the array so that all elements less than the pivot come before it and all elements greater than the pivot come after it. It returns the index of the pivot element.
     def partition(zip_codes, low, high, zip_codes_with_distance)
       pivot = zip_codes[high]
       i = low - 1
@@ -97,12 +113,13 @@ class ZipCodeEndpoint
       i + 1
     end
 
+    # This private method compares the tier values of two zip codes. It takes two parameters: `zip1` and `zip2` (zip code objects). It uses the `TIER_VALUES` constant to determine the order of the tiers and returns true if `zip1` has a lower tier than `zip2`.
     def compare_zip_tier(zip1, zip2)
       TIER_VALUES.index(zip1.tier) < TIER_VALUES.index(zip2.tier)
     end
 
+    # This private method compares the distances of two zip codes. It takes three parameters: `zip1` and `zip2` (zip codes as strings) and `zip_codes_with_distance` (a hash mapping zip codes to distances). It uses the `zip_codes_with_distance` hash to retrieve the distances for the zip codes and returns true if the distance of `zip1` is greater than the distance of `zip2`.
     def compare_zip_distances(zip1, zip2, zip_codes_with_distance)
-      # use zip_codes as keys to get the distances from zip_codes_with_distance
       zip_codes_with_distance[zip1] > zip_codes_with_distance[zip2]
     end
   end
